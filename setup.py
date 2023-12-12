1fe2de5ee518ab4dec03b3fa93c1aa9949322038
Florian Fervers <florian.fervers@gmail.com>
2023-12-12 22:07:36 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="tinylogdir",
-    version="0.1.0",
+    version="0.1.1",
     description="A lightweight library for creating output directories of python scripts.",
     author="Florian Fervers",
     author_email="florian.fervers@gmail.com",
